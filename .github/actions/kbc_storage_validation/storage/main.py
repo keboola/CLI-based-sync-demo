571bc11b56df9c16b5cd143f5d3ac44a663de9b4
ZdenekSrotyr <zdenek.srotyr@keboola.com>
2024-06-24 16:49:54 +0200

    validation

--- .github/actions/kbc_storage_validation/storage/main.py
+++ .github/actions/kbc_storage_validation/storage/main.py
@@ -30,6 +30,9 @@ def check_parameters():
 
 if __name__ == '__main__':
     args = check_parameters()
+    print("token_here")
+    print(args.source_token[3:4])
+    print(args.dest_token[3:4])
     StorageDiff(
         source_workdir=args.source_workdir,
         source_host=args.source_host,
