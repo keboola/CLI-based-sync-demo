2290e369a1d3408456a0db5d88e0311ae419d2d6
ZdenekSrotyr <zdenek.srotyr@keboola.com>
2024-06-24 12:19:46 +0200

    validation on the end

--- .github/actions/kbc_storage_validation/pull/storage_pull.py
+++ .github/actions/kbc_storage_validation/pull/storage_pull.py
@@ -15,6 +15,7 @@ class StoragePull:
             return ''.join(['https://', host])
         return host
 
+    # TODO: change to list buckets and then tables
     def _get_tables(self):
         tables = requests.get(url=''.join([self._base, '/v2/storage/tables']), headers=self._head,
                               params={'include': 'buckets,columns,metadata,columnMetadata'})
@@ -29,3 +30,15 @@ class StoragePull:
 
     def pull(self):
         return self._save_storage_structure(self._get_tables())
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Pull storage structure from KBC')
+    parser.add_argument('--host', required=True, help='KBC host')
+    parser.add_argument('--token', required=True, help='KBC token')
+    parser.add_argument('--destination-file', required=True, help='Destination file')
+    args = parser.parse_args()
+
+    StoragePull(args.host, args.token, args.destination_file).pull()
